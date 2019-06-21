61b3397ac4492c8e33fe9be3493753a9acca927d
Ovidio R. Calvet <calveto@gmail.com>
2019-06-21 15:56:31 -0400

    Build app with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     main: './src/index.js'
   },
   output: {
     filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'VanillaWeb - Webpack',
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    port: 9081
   }
 };
