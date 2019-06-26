78eefd9eceb46cd0c2b7d734d6be07dfb5b7f5b5
Ovidio R. Calvet <calveto@gmail.com>
2019-06-26 16:24:48 -0400

    Fix bug - app was not loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 export default (document = window.document) => {
   return {
     init(selector = '#root') {
-      const queryResult = document.querySelector(selector);
-      console.log(queryResult);
-      if (queryResult) {
-        const element = queryResult;
+      const container = document.querySelector(selector);
+      if (container) {
         const app = document.createElement('div');
         app.innerText = 'Application Loaded';
-        app.appendChild(element);
+        container.appendChild(app);
       } else {
         throw Error(`Cannot find element with selector ${selector}`);
       }
