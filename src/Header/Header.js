1ce36db2a732efa66e8803bf057f049aad742f9e
Ovidio R. Calvet <calveto@gmail.com>
2019-07-04 01:11:14 -0400

    Remove unneccesary comments

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -10,22 +10,4 @@ export default root => {
   mobileNavigation.appendChild(logoLink);
   root.appendChild(nav);
   return true;
-  //   <nav>
-  //     <div class="nav-wrapper">
-  //       <a href="#" class="brand-logo">
-  //         Logo
-  //       </a>
-  //       <ul id="nav-mobile" class="right hide-on-med-and-down">
-  //         <li>
-  //           <a href="sass.html">Sass</a>
-  //         </li>
-  //         <li>
-  //           <a href="badges.html">Components</a>
-  //         </li>
-  //         <li>
-  //           <a href="collapsible.html">JavaScript</a>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   </nav>;
 };
