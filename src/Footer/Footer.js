d471d87de5bdd14b1c1c01c15a78a9e19f76404e
Ovidio R. Calvet <calveto@gmail.com>
2019-07-05 01:15:25 -0400

    Wrapper footer in container

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,7 @@
 export default root => {
   const footer = window.document.createElement('footer');
   const footerWrapper = window.document.createElement('div');
+  footerWrapper.className = 'container';
   footerWrapper.innerText = 'Copyright';
   footer.appendChild(footerWrapper);
   root.appendChild(footer);
