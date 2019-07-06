e6e814342bd5b9c1ce9dffc1097300f5102f5973
Ovidio R. Calvet <calveto@gmail.com>
2019-07-06 01:27:59 -0400

    Add footer copyright area

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,14 @@
 export default root => {
   const footer = window.document.createElement('footer');
+  footer.className = 'page-footer';
   const footerWrapper = window.document.createElement('div');
   footerWrapper.className = 'container';
-  footerWrapper.innerText = 'Copyright';
+  footerWrapper.innerText = 'Footer content';
+  const footerCopyright = document.createElement('div');
+  footerCopyright.className = 'footer-copyright';
+  footerCopyright.innerText = '© 2014 Copyright Text';
   footer.appendChild(footerWrapper);
+  footer.appendChild(footerCopyright);
   root.appendChild(footer);
   return true;
 };
